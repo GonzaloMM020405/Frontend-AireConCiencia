cb34fcc2d00f727dd450d976fc4403a90d9a4d13
GonzaloMM <gonzalomm1@outlook.com>
2025-10-05 09:06:55 -0600

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <AirQualityMonitor />
+  </div>
+</template>
+
+<script>
+import AirQualityMonitor from './components/AirQualityMonitor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AirQualityMonitor
+  }
+}
+</script>
+
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  min-height: 100vh;
+background: linear-gradient(100deg, #b5e2fa 0%, #f9f7f3 100%);}
+
+#app {
+  min-height: 100vh;
+}
+</style>
